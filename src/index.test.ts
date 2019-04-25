9f7d9ff3a6afb88ba0808567ac882a535e6a6586
jschloer <jschloer@hotmail.com>
2019-04-24 23:17:58 -0400

    Inital functions working...sortof

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,20 @@
-import { Test } from './index';
-test('My Greeter', () => {
-  expect(Test('Carl')).toBe('Hello Carl');
+import { fractionToDecimal, stringToFraction } from './index';
+describe('fractionToDecimal', () => {
+  test('1100', () => {
+    expect(fractionToDecimal('1100')).toBe(12);
+  });
+  test('0.0011', () => {
+    expect(fractionToDecimal('0.0011')).toBe(0.1875);
+  });
+  test('-1100.0011', () => {
+    expect(fractionToDecimal('-1100.0011')).toBe(-12.1875);
+  });
+});
+describe('stringToFractions', () => {
+  test('4', () => {
+    expect(stringToFraction('4')).toBe('100');
+  });
+  xtest('4 1/2', () => {
+    expect(stringToFraction('4 1/2')).toBe('100.1');
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-export const Test = (name: string) => `Hello ${name}`;
+export const fractionToDecimal = (fraction: string): number => {
+  // split the string into  two
+  let [leftSide, rightSide] = fraction.trim().split('.');
+  let base = 2;
+
+  return (
+    (parseInt(leftSide.replace('-', ''), base) +
+      (rightSide || '').split('').reduceRight((sum, bit) => (sum + parseInt(bit, base)) / base, 0)) *
+    (fraction[0] === '-' ? -1 : 1)
+  );
+};
+export const stringToFraction = (fractionString: string): string => {
+  let nums = fractionString.split(' ');
+  let leftSide = '';
+  let rightSide = '';
+  let number = 0;
+  let fraction = '';
+  if (nums.length > 1) {
+    number = Number(nums[0]);
+    fraction = nums[1];
+  } else if (nums.length === 1) {
+    if (nums[0].indexOf('/') >= 0) {
+      fraction = nums[0];
+      let [numerator, denominator] = fraction.split('/');
+      let index = 0;
+    } else {
+      number = Number(nums[0]);
+    }
+  }
+
+  // convert number to binary
+  leftSide = number.toString(2);
+
+  return `${leftSide}${rightSide && '.' + rightSide}`;
+};
