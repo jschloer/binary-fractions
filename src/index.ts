314190c98224dfd4b2b83583d8715199d6fe5279
jschloer <jschloer@hotmail.com>
2019-04-24 23:20:09 -0400

    Not sure I like tslint...

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export const fractionToDecimal = (fraction: string): number => {
   // split the string into  two
-  let [leftSide, rightSide] = fraction.trim().split('.');
-  let base = 2;
+  const [leftSide, rightSide] = fraction.trim().split('.');
+  const base = 2;
 
   return (
     (parseInt(leftSide.replace('-', ''), base) +
@@ -10,26 +10,26 @@ export const fractionToDecimal = (fraction: string): number => {
   );
 };
 export const stringToFraction = (fractionString: string): string => {
-  let nums = fractionString.split(' ');
+  const nums = fractionString.split(' ');
   let leftSide = '';
-  let rightSide = '';
-  let number = 0;
+  const rightSide = '';
+  let num = 0;
   let fraction = '';
   if (nums.length > 1) {
-    number = Number(nums[0]);
+    num = Number(nums[0]);
     fraction = nums[1];
   } else if (nums.length === 1) {
     if (nums[0].indexOf('/') >= 0) {
       fraction = nums[0];
-      let [numerator, denominator] = fraction.split('/');
-      let index = 0;
+      const [numerator, denominator] = fraction.split('/');
+      const index = 0;
     } else {
-      number = Number(nums[0]);
+      num = Number(nums[0]);
     }
   }
 
   // convert number to binary
-  leftSide = number.toString(2);
+  leftSide = num.toString(2);
 
   return `${leftSide}${rightSide && '.' + rightSide}`;
 };
